ecc561c0f8eb39a2be859e8b011cd076cac832f4
kris15putrach <krisnapr43@gmail.com>
2024-04-13 11:32:08 +0700

    Menambahkan Model Akun untuk tabel akun

---
+++ app/Models/Akun.php
@@ -0,0 +1,18 @@
+// app/Models/Akun.php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Akun extends Model
+{
+    use HasFactory;
+
+    protected $table = 'akun';
+
+    protected $fillable = [
+        'username',
+        'password',
+    ];
+}
