2cf3057a76d7655855671f144fc62c9e885d1a61
kris15putrach <krisnapr43@gmail.com>
2024-04-15 14:17:27 +0700

    tampilanutama

---
+++ resources/views/tampilanutama.php
@@ -0,0 +1,59 @@
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Nihao! Selamat Datang</title>
+        <link href="tampilanutama/tampilanutama.css" rel="stylesheet">
+        <link href="/bootstrap-5.3.3-dist/css/bootstrap.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+        <style>
+
+        </style>
+    </head>
+
+    <body>
+
+        <header>
+            <div class="logo">
+                <img src="tampilanutama/headercamera.png" alt="NIHAO Logo">
+                <h1>NiHaO</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#blog">Blog</a></li>
+                    <li><a href="#service">Service</a></li>
+                    <li><a href="#about">About</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <div class="container">
+            <div class='selamatdatang'>
+            <p><b>Hallo selamat datang</b></p>
+
+                <div class="textmari">
+                    <h1><b>Mari kita analisis</b></h1>
+                    <h1><b>Ikan nilamu!!!</b></h1>
+
+                    <div class="testanalisi">
+                        <p><b>Analisislah sesuai dengan keinginan hati, daftar dan dapatkan fitur menarik lainnya.</b></p>
+                    
+                        <div class="buttons">
+                                <button><b>Registrasi</b></button>
+                                <button><b>Login</b></button>
+                        </div>
+                </div>            
+            </div>  
+        </div>
+
+        <div class="kamerakanan">
+            <img src="\tampilanutama\kamerakanan.png" alt="Deskripsi gambar">
+        </div>
+        
+        <script src="/bootstrap-5.3.3-dist/js/bootstrap.js"></script>
+    </body>
+
+    </html>
--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/home', function () {
-    return view('home');
+    return view('tampilanutama');
     
 });
 
 Route::get('/daftar', function () {
     return view('daftar');
 });
-
 
 use App\Http\Controllers\AkunController;
 
