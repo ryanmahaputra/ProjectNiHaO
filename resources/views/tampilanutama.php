08b05a8f8f9d4d845d5f974860519485836a8607
kris15putrach <krisnapr43@gmail.com>
2024-04-18 23:34:50 +0700

    css

--- resources/views/tampilanutama.php
+++ resources/views/tampilanutama.php
@@ -1,25 +1,23 @@
-<!DOCTYPE html>
-        <html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-        <head> 
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Nihao! Selamat Datang</title>
-            <link href="\header\header.css" rel="stylesheet">
-            <link href="/bootstrap-5.3.3-dist/css/bootstrap.css" rel="stylesheet">
-            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-            <script src="\header\header.js"></script>
-            <script src="/bootstrap-5.3.3-dist/js/bootstrap.js"></script>
-            <style>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Nihao! Selamat Datang</title>
+        <link href="tampilanutama/tampilanutama.css" rel="stylesheet">
+        <link href="/bootstrap-5.3.3-dist/css/bootstrap.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+        <style>
 
-            </style>
-        </head>
+        </style>
+    </head>
 
-        <body>
+    <body>
 
-            <header>
-                    <div class="logo">
-                    <img src="tampilanutama/headercamera.png" alt="NiHao Logo">
+    <header>
+                <div class="logo">
+                    <img src="tampilanutama/headercamera.png" alt="NIHAO Logo">
                     <h1>NiHaO</h1>
                 </div>
                 <nav>
@@ -42,19 +40,19 @@
                     <h1><b>Mari kita analisis</b></h1>
                     <h1><b>Ikan nilamu!!!</b></h1>
 
-                    <div class="testanalisis">
+                    <div class="testanalisi">
                         <p><b>Analisislah sesuai dengan keinginan hati, daftar dan dapatkan fitur menarik lainnya.</b></p>
                         <p><b>
                         <div class="buttons">
-                        <a href="daftar.blade.php"><button><b>Registrasi</b></button></a>
-                        <a href="login.blade.php"><button><b>Login</b></button></a>
+                                <button><b>Registrasi</b></button>
+                                <button><b>Login</b></button>
                         </div>
                 </div>            
             </div>  
         </div>
     
         <div class="kamerakanan">
-            <img src="tampilanutama\kamerakanan.png" alt="Deskripsi gambar">
+            <img src="\tampilanutama\kamerakanan.png" alt="Deskripsi gambar">
         </div>
     </div>  
         <script src="/bootstrap-5.3.3-dist/js/bootstrap.js"></script>
